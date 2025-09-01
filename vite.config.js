ca0296696e31ba32048053e78c1172c36ff6db51
Dasanapu Vaishnavi <dasanapuvaishnavi@Mac.attlocal.net>
2025-09-01 16:24:51 -0700

    pkg changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/paradise-nursery-shopping-cart-app",
+    plugins: [react()],
 })
